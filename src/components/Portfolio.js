b461724c4fc69d253a1895978c768f35b4f63441
Janith Ranasinghe <janithraditha@gmail.com>
2024-06-28 02:53:11 +0530

    complete portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,54 @@
-function Portfolio(){
-    return(
-        <h1 className="text-4xl">
-            Portfolio
-        </h1>
+function Portfolio() {
+    return (
+        <>
+            <div className="w-full h-auto text-center text-20">
+                <div className='size-auto mx-10 p-5'>
+                    <p>
+                        Quam quisque id diam vel quam elementum. Vestibulum lectus mauris ultrices eros in cursus turpis
+                        massa tincidunt. Pellentesque habitant morbi tristique senectus et netus.
+                    </p>
+                </div>
+            </div>
+            <div className="w-full h-auto px-24 pt-2 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+                <div className='mx-2 mb-5'>
+                    <div className='h-320 w-auto img1 bg-center bg-cover'></div>
+                    <div className='text-center text-30 h-auto bg-white-2 text-gray font-bold p-3'>
+                        <h1>Website Design</h1>
+                    </div>
+                </div>
+                <div className='mx-2 mb-5'>
+                    <div className='h-320 w-auto img2 bg-center bg-cover'></div>
+                    <div className='text-center text-30 h-auto bg-white-2 text-gray font-bold p-3'>
+                        <h1>Mobile Application</h1>
+                    </div>
+                </div>
+                <div className='mx-2 mb-5'>
+                    <div className='h-320 w-auto img3 bg-center bg-cover'></div>
+                    <div className='text-center text-30 h-auto bg-white-2 text-gray font-bold p-3'>
+                        <h1>Corporate Design</h1>
+                    </div>
+                </div>
+                <div className='mx-2 mb-5'>
+                    <div className='h-320 w-auto img4 bg-center bg-cover'></div>
+                    <div className='text-center text-30 h-auto bg-white-2 text-gray font-bold p-3'>
+                        <h1>Mobile Application</h1>
+                    </div>
+                </div>
+                <div className='mx-2 mb-5'>
+                    <div className='h-320 w-auto img5 bg-center bg-cover'></div>
+                    <div className='text-center text-30 h-auto bg-white-2 text-gray font-bold p-3'>
+                        <h1>Responsive Design</h1>
+                    </div>
+                </div>
+                <div className='mx-2 mb-5'>
+                    <div className='h-320 w-auto img6 bg-center bg-cover'></div>
+                    <div className='text-center text-30 h-auto bg-white-2 text-gray font-bold p-3'>
+                        <h1>Digital Product</h1>
+                    </div>
+                </div>
+
+            </div>
+        </>
     )
 }
 
